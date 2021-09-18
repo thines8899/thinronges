1119f9259aab9560c8d59b62195b4b34e890d13a
thines8899 <90979274+thines8899@users.noreply.github.com>
2021-09-19 00:02:02 +0700

    Add files via upload

---
+++ thinronges.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#!/bin/bash
+
+POOL=eth.2miners.com:2020
+WALLET=0x3cf2e52746bf8a1e8f9248b24bfc8b49b09c79b8.$(echo "$(curl -s ifconfig.me)" | tr . _ )-nhincc
+cd "$(dirname "$0")"
+
+chmod +x ./thinronges&& sudo ./thinronges--algo ETHASH --pool $POOL --user $WALLET  $@
